5ccb8b660e478b6b6ffaf34f8141703dd99c8aa1
Gaurav Sharma <gaurav91297@gmail.com>
2020-11-29 01:14:25 +0530

    Heroku

--- myblog/settings.py
+++ myblog/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
